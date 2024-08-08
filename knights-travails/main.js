cbc21ec56d038a06723030616122835198ce3f2b
thomasmfx <thomasmoisesf@gmail.com>
2024-08-08 16:59:20 -0300

    feat: Add class Board and function to build a matrix to represent the board

--- knights-travails/main.js
+++ knights-travails/main.js
@@ -1,3 +1,29 @@
+function buildAdjMatrix(knightPosition) {
+  let matrix = [];
+  let row = [];
+  let x = knightPosition[0];
+  let y = knightPosition[1];
+
+  while (row.length < 8) {
+    row.push(0);
+  };
+  while (matrix.length < 8) {
+    matrix.push(row);
+  };
+
+  return matrix;
+};
+
+class Board {
+  constructor(initialPosition = [4, 4]) {
+    this._matrix = buildAdjMatrix(initialPosition);
+  };
+
+  get matrix() {
+    return this._matrix;
+  };
+};
+
 function knightMoves(x, y) {
 
 };
