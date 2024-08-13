cb7456fbf6c4aa32de5d0a533de43a89ea3b0657
thomasmfx <thomasmoisesf@gmail.com>
2024-08-13 11:35:39 -0300

    refactor: Update remotion of functions that were moved to Board class

--- knights-travails/matrix.js
+++ knights-travails/matrix.js
@@ -7,34 +7,10 @@ export function outOfBoard(cordinates) {
 
 export function buildMatrix() {
   let matrix = [];
-  let x = knightPosition[0];
-  let y = knightPosition[1];
 
   while (matrix.length < 8) {
     matrix.push([0, 0, 0, 0, 0, 0, 0, 0]);
   };
 
   return matrix; 
-};
-
-    [x - 2, y + 1],
-    [x + 1, y - 2],
-
-    [x - 1, y + 2],
-    [x + 2, y - 1],
-
-    [x + 1, y + 2],
-    [x + 2, y + 1]
-  ];
-
-  for(let i = 0; i < positions.length; i++) {
-    let xAxis = positions[i].shift();
-    let yAxis = positions[i].shift();
-
-    if (outOfBoard(xAxis) || outOfBoard(yAxis)) continue;
-
-    matrix[xAxis][yAxis] = 1;
-  };
-
-  return matrix;
 };
