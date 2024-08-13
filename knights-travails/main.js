b6211c6378c41df9446493110cff5a440450a356
thomasmfx <thomasmoisesf@gmail.com>
2024-08-13 11:33:47 -0300

    feat: Add functions to compare positions and print steps from start to end

--- knights-travails/main.js
+++ knights-travails/main.js
@@ -1,4 +1,22 @@
-import buildMatrix from "./matrix.js";
+import { buildMatrix, outOfBoard } from "./matrix.js";
+
+function equalPositions(p1, p2) {
+  if ((p1[0] === p2[0]) && (p1[1] === p2[1])) {
+    return true
+  };
+
+  return false;
+};
+
+function printSteps(steps) {
+  let text = `You made it in ${steps.length - 1} moves! Here's your path: \n`;
+
+  for (const step of steps) {
+    text += ` [${step}] \n`
+  };
+
+  return text;
+};
 
 class Board {
   constructor() {
